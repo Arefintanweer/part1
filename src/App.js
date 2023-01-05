df85a6b4e3365a84bc8cfd96b4067d6b18b01f09
Arefintanweer <arefintanweer@gmail.com>
2023-01-04 20:06:16 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//this is a multiple component
+const Hello = (props) =>{
+  return(
+    <div>
+      <h3>This is another component; named Hello</h3>
+      <p>catching props {props.name}</p>
     </div>
-  );
+  )
+}
+
+const App = () => {
+
+  console.log('Heya! Component talking from console! JS code in component stays above the return keywords')
+
+  const a = 5
+  const b = 10
+
+  return (
+   <div>
+    <h1>This is part 1a- Intro to React </h1>
+    <p>
+      we are learning about: 
+      
+        1. component : arrow function, app component
+        2. JSX : JavaScript in HTML
+        3. multiple components : more components in app componet
+        4. Props : Passing data to components
+
+    </p>
+    <p>executing js code inside component: {a} +{b} = {a+b}</p>
+    <Hello name= 'john'></Hello>
+    <Hello name = 'smith'/>
+
+    <h1> This is part 1b- JavaScript</h1>
+    <p>
+      1. Variables
+      2. Arrays
+      3. Objects
+      4. Functions
+
+    </p>
+    <h1>This is part 1c- State and event handlers </h1>
+
+    <p></p>
+
+   </div> 
+  
+  )
 }
 
 export default App;
